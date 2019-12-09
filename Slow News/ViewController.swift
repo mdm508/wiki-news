a84116b1f10fcd440f8b6dea580c5c35c052488f
Matthew McLaughlin <matthedm@uci.edu>
2019-12-09 10:21:50 -0800

    Initial Commit

---
+++ Slow News/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Slow News
+//
+//  Created by Matthew McLaughlin on 12/9/19.
+//  Copyright © 2019 Matthew McLaughlin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
